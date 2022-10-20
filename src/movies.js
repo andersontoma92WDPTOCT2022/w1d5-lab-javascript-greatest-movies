4d4046e9ae765483c3d9709459dec4fc934cde2b
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-10-20 16:55:37 -0300

    1 to 4

--- src/movies.js
+++ src/movies.js
@@ -8,25 +8,69 @@ function getAllDirectors(moviesArray) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-  console.log(typeof moviesArray, '< tipo');
-  console.log(moviesArray, '< - print');
-
   let filtredArr = moviesArray.filter(
     (elemento) =>
       elemento.director === 'Steven Spielberg' &&
       elemento.genre.includes('Drama')
   );
-  console.log(filtredArr.length, '<--it 2');
+  //console.log(filtredArr.length, '<--it 2');
   return filtredArr.length;
 }
 
+const moviesTeste = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 8,
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 'fsadf',
+  },
+];
+
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {
+function scoresAverage(moviesArray, eFloat) {
   if (moviesArray.length === 0) return 0;
+  let noNumber = 0;
+  let somaScore = moviesArray.reduce((accumulator, currentObject) => {
+    if (typeof currentObject.score === 'number') {
+      return accumulator + currentObject.score;
+    } else {
+      noNumber++;
+      return accumulator;
+    }
+  }, 0);
+  /*console.log(
+    Math.round(100 * (somaScore / (moviesArray.length - noNumber))) / 100);   mais certo seria excluir os casos q não são números*/
+  // para reutilizar a função
+  console.log(typeof eFloat, ' éfloat');
+  if (eFloat == undefined) {
+    return Math.round((100 * somaScore) / moviesArray.length) / 100;
+  }
+  return somaScore / moviesArray.length;
+
+  // arredonda antes de retornar
 }
 
+//console.log(scoresAverage(moviesTeste), '<-average');
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const dramaMoviesArray = moviesArray.filter((cadaObjeto) =>
+    cadaObjeto.genre.includes('Drama')
+  );
+  //console.log(scoresAverage(dramaMoviesArray, true), '<float');
+  // incluido o efloat no scoresAverage(,eFloat) para reaproveitar codigo
+  return scoresAverage(dramaMoviesArray, true);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
